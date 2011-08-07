d6776e143e47e8b187dad68f4709f1260b343296
ianmonge <ianmonge@00ca4a15-86cb-4c48-9526-8fcadc9b7c12>
2011-08-07 22:06:57 +0000

    Task Request class creada y testeada
    git-svn-id: https://subversion.assembla.com/svn/ianmonge/tests/task@421 00ca4a15-86cb-4c48-9526-8fcadc9b7c12

--- scripts/report.php
+++ scripts/report.php
@@ -1,10 +1,26 @@
 <?php 
 
 require_once 'Bootstrap.php';
+require_once 'Request.php';
+
 $bootstrap = new Bootstrap();
 $bootstrap->init();
+
+$request = new Request();
+try
+{
+    $merechantId = $request->getMerchantId();
+}
+ catch ( Exception $e )
+ {
+     echo $e->getMessage() . PHP_EOL;
+     echo <<<HEREDOC
+   php report.php <integer>
+
+HEREDOC;
+     exit( 1 );
+ }
 
 //foreach ($merchant->getTransactions() as $transaction) {
 //    
 //}
-echo "END\n";
